0e276d16471ae259c09d73639e474bfd6ba21e5c
Juamops Mopea <juampsyololol@gmail.com>
2020-08-21 15:29:02 -0500

    Added Periods CMD

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime as dt
 
 
-token = 'NzQ1NjgzNDI0MjE1NDMzMzE5.Xz1Vzg.UH2kyuypz6TQh7UnsSHVhI4cfGw'
+token = 'INSERT_TOKEN_HERE'
 client = commands.Bot(command_prefix='#')
 client.remove_command('help')
 
